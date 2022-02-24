d332f8ec5053b2851722402928fd76eb1b6d9c11
VasiliB <73487624+VasiliB@users.noreply.github.com>
2022-02-25 00:43:53 +0300

    1 solved

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,27 @@
 package subtask1
 
-class DateFormatter {
+import java.text.SimpleDateFormat
 
+import java.util.*
+
+class DateFormatter {
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val error = ("Такого дня не существует")
+        val myDate = Calendar.getInstance()
+        myDate.set(
+            year.toInt(),
+            month.toInt() - 1,
+            day.toInt()
+        )
+        val dayOutFormat = SimpleDateFormat("d M, yyyy", Locale("RU"))
+        val dayIn = ("$day $month, $year")
+        val dayOut = (dayOutFormat.format(myDate.getTime())).toString()
+        if (dayIn == dayOut) {
+            val fullDate = SimpleDateFormat("d MMMM, EEEE", Locale("RU"))
+            val result = (fullDate.format(myDate.getTime()))
+            println(fullDate.format(myDate.getTime()))
+            return (result)
+        } else return (error)
     }
 }
