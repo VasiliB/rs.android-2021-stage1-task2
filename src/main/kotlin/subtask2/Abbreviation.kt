bd4aa9efa64150effeb89170a2a3c5f3017f3070
VasiliB <73487624+VasiliB@users.noreply.github.com>
2022-03-02 01:41:42 +0300

    2 solved

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,35 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var indexA = 0
+        var indexB = 0
+        var bufferIndex = 1
+        var buffer = ""
+        lateinit var answer: String
+        val y = "YES"
+        val n = "NO"
+        while (indexA <= a.lastIndex) {
+            if (a[indexA] != b[indexB] && a[indexA].isUpperCase()) {
+                answer = n
+                println(answer)
+            } else {
+                if (a[indexA].toLowerCase() == b[indexB].toLowerCase()) {
+                    buffer = buffer.padEnd(bufferIndex, a[indexA])
+                    if (indexB != b.lastIndex && indexA != a.lastIndex) {
+                        indexB++
+                        bufferIndex++
+                    }
+                }
+            }
+            indexA++
+        }
+        if (buffer.toUpperCase() == b) {
+            answer = y
+        } else {
+            answer = n
+        }
+        println(buffer)
+        println(b)
+        return (answer)
     }
 }
